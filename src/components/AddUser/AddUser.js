2803333ef504aa1ebc9107919a18827bdfea7bd5
unknown <sifatniloy18@gmail.com>
2022-10-14 23:15:50 -0600

    new react app for mongo practise

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+
+const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleAddUser = e => {
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const newUser = { name, email }
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('user added successfully');
+                    e.target.reset();
+                }
+            })
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <h2>Please Add an User</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="" ref={nameRef} id="" placeholder='name' />
+                <input type="email" name="" ref={emailRef} id="" placeholder='email' />
+                <input type="submit" value="Add" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
